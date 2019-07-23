1730ac5ce9c93ceaa8ab9c8b2eb7ad84000e6581
Matt Bell <matthewaustinbell@gmail.com>
2019-07-23 16:42:42 -0500

    log out button is working

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import firebase from 'firebase/app';
 
 import Auth from '../components/Auth/Auth';
 import Home from '../components/Home/Home';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 
 import './App.scss';
 
@@ -30,16 +31,19 @@ class App extends React.Component {
   }
 
   render() {
+    const { authed } = this.state;
     const loadComponent = () => {
-      if (this.state.authed) {
+      if (authed) {
         return <Home />;
       }
-      return <Auth />
+      return <Auth />;
     };
 
     return (
       <div className="App">
+         <MyNavbar authed={authed} />
         {loadComponent()}
+        <Auth />
       </div>
     );
   }
