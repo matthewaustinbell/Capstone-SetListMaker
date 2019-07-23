0dcadbf7c0ce2116c477f8338fb18309e758c14a
Matt Bell <matthewaustinbell@gmail.com>
2019-07-23 13:39:57 -0500

    added home & auth components

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className='btn btn-danger'>HELP ME</button>
-      </header>
-    </div>
-  );
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="btn btn-danger">Set List Maker</button>
+      </div>
+    );
+  }
 }
 
 export default App;
