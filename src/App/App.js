99d0c18381a47babd66a2aa44d5bc4a39988c4ee
Matt Bell <matthewaustinbell@gmail.com>
2019-07-23 13:47:08 -0500

    auth and home showing

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+
 import './App.scss';
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <button className="btn btn-danger">Set List Maker</button>
+        <Auth />
+        <Home />
       </div>
     );
   }
