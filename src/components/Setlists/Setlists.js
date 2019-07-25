8ef46a5130bf7509d52d854d6bf9e4679e9f4950
Matt Bell <matthewaustinbell@gmail.com>
2019-07-25 11:08:47 -0500

    wip

--- src/components/Setlists/Setlists.js
+++ src/components/Setlists/Setlists.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import setlistsData from '../../helpers/data/setlistsData';
 
 import './Setlists.scss';
 
 class Setlists extends React.Component {
+  state = {
+    orders: [],
+  }
+
+  componentDidMount() {
+    setlistsData.getMySetlists(firebase.auth().currentSetlist.uid)
+      .then(orders => this.setState({ orders }))
+      .catch(err => console.error('cant get orders', err));
+  }
+
   render() {
     return (
       <div className="Setlists">
